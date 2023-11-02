3774f1da82c380c993224cf0eab8ddc6037c0c40
Austin Farr <austin.farr@capco.com>
2023-11-02 00:01:46 -0500

    Broke home page into smaller components and made performance optimizations

---
+++ src/components/home/SearchBar.js
@@ -0,0 +1,41 @@
+import { Search } from "@mui/icons-material";
+import { InputAdornment, TextField } from "@mui/material";
+
+function SearchBar({ searchQuery, onSearchChange }) {
+  return (
+    <TextField
+      fullWidth
+      sx={{
+        "& .MuiOutlinedInput-root": {
+          height: "100%",
+          borderRadius: 0.5,
+          backgroundColor: "rgb(239, 245, 249)",
+          border: "none",
+          "& fieldset": {
+            border: "none", // Remove border
+          },
+          "&:hover fieldset": {
+            border: "none", // Remove border on hover
+          },
+          "&.Mui-focused fieldset": {
+            border: "none", // Remove border when focused
+          },
+        },
+      }}
+      variant="outlined"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      // placeholder="Search for items..."
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Search />
+          </InputAdornment>
+        ),
+        style: { backgroundColor: "#e8e8e8" },
+      }}
+    />
+  );
+}
+
+export default SearchBar;
