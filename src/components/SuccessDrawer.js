d05b8611b2327702d1cf425254e9231182fb3f83
Austin Farr <austin.farr@capco.com>
2023-11-12 20:34:05 -0600

    Added confetti

--- src/components/SuccessDrawer.js
+++ src/components/SuccessDrawer.js
@@ -4,10 +4,21 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { useDrawer } from "@/context/DrawerContext";
+import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
 const SuccessDrawer = () => {
   const { purchaseSuccess, boughtItem, hideSuccessMessage } = usePurchase();
 
   const router = useRouter();
+
+  const [confettiActive, setConfettiActive] = useState(false);
+
+  useEffect(() => {
+    // Activate the confetti when purchaseSuccess becomes true
+    if (purchaseSuccess) {
+      setConfettiActive(true);
+    }
+  }, [purchaseSuccess]);
 
   const handleGoToPurchases = () => {
     hideSuccessMessage();
@@ -17,91 +28,97 @@ const SuccessDrawer = () => {
   console.log("boughtItem", boughtItem);
 
   return (
-    <Drawer
-      anchor="bottom"
-      open={purchaseSuccess}
-      onClose={hideSuccessMessage}
-      sx={{
-        height: "60%",
-        display: "flex",
-        flexDirection: "column",
-        "& .MuiPaper-root": {
-          borderTopLeftRadius: 32,
-          borderTopRightRadius: 32,
-          height: "60%",
-          background: "#fff",
-        },
-      }}
-    >
-      <Box
-        p={3}
+    <>
+      <Drawer
+        anchor="bottom"
+        open={purchaseSuccess}
+        onClose={() => {
+          hideSuccessMessage();
+          setConfettiActive(false); // Stop the confetti when drawer closes
+        }}
         sx={{
+          height: "60%",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100%", // Take up all available space in the drawer
+          "& .MuiPaper-root": {
+            borderTopLeftRadius: 32,
+            borderTopRightRadius: 32,
+            height: "60%",
+            background: "#fff",
+          },
         }}
       >
+        {confettiActive && <Confetti />}
         <Box
+          p={3}
           sx={{
-            position: "relative",
             display: "flex",
-            justifyContent: "center",
+            flexDirection: "column",
             alignItems: "center",
-            borderRadius: "5%",
-            overflow: "hidden",
-            height: "150px",
-            width: "200px",
-            //   width: 400,
-            margin: "0 auto",
-            mb: 3,
+            justifyContent: "center",
+            height: "100%", // Take up all available space in the drawer
           }}
         >
-          <Image
-            src={boughtItem?.image?.images[0]}
-            // width={200}
-            // height={200}
-            alt="item image"
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{ objectFit: "cover" }}
-          />
+          <Box
+            sx={{
+              position: "relative",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "5%",
+              overflow: "hidden",
+              height: "150px",
+              width: "200px",
+              //   width: 400,
+              margin: "0 auto",
+              mb: 3,
+            }}
+          >
+            <Image
+              src={boughtItem?.image?.images[0]}
+              // width={200}
+              // height={200}
+              alt="item image"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              style={{ objectFit: "cover" }}
+            />
+          </Box>
+          <Typography variant="h5" sx={{ fontWeight: 600, fontSize: 28 }}>
+            Purchase Successful!
+          </Typography>
+          <Typography>{`You purchased ${
+            boughtItem?.title
+          } for $${boughtItem?.buy_now_price.toLocaleString()}.`}</Typography>
+
+          <Typography
+            onClick={handleGoToPurchases}
+            sx={{
+              textDecoration: "underline",
+              textDecorationThickness: "1px", // Adjust thickness of the underline
+              textUnderlineOffset: "4px",
+              cursor: "pointer",
+              color: "#13294B",
+              mb: 5,
+            }}
+          >
+            View your purchases here.
+          </Typography>
+
+          <Button
+            onClick={hideSuccessMessage}
+            sx={{
+              //   margin: "0 auto",
+              textDecoration: "underline",
+              textDecorationThickness: "2px", // Adjust thickness of the underline
+              textUnderlineOffset: "4px",
+            }}
+          >
+            Close
+          </Button>
         </Box>
-        <Typography variant="h5" sx={{ fontWeight: 600, fontSize: 28 }}>
-          Purchase Successful!
-        </Typography>
-        <Typography>{`You purchased ${
-          boughtItem?.title
-        } for $${boughtItem?.buy_now_price.toLocaleString()}.`}</Typography>
-
-        <Typography
-          onClick={handleGoToPurchases}
-          sx={{
-            textDecoration: "underline",
-            textDecorationThickness: "1px", // Adjust thickness of the underline
-            textUnderlineOffset: "4px",
-            cursor: "pointer",
-            color: "#13294B",
-            mb: 5,
-          }}
-        >
-          View your purchases here.
-        </Typography>
-
-        <Button
-          onClick={hideSuccessMessage}
-          sx={{
-            //   margin: "0 auto",
-            textDecoration: "underline",
-            textDecorationThickness: "2px", // Adjust thickness of the underline
-            textUnderlineOffset: "4px",
-          }}
-        >
-          Close
-        </Button>
-      </Box>
-    </Drawer>
+      </Drawer>
+    </>
   );
 };
 
