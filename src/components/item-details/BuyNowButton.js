7340f09071093a023424f7fa5017eadb1ba33f00
Austin Farr <austin.farr@capco.com>
2023-12-08 12:35:24 -0600

    Bug fixes for missing fields in DB

--- src/components/item-details/BuyNowButton.js
+++ src/components/item-details/BuyNowButton.js
@@ -1,39 +1,47 @@
 import { Box, Button } from "@mui/material";
 
 function BuyNowButton({ item, user, onBuyNowClick }) {
+  // let buyNowPrice = "No Price";
+
+  // if (item.buyNowPrice) {
+  //   buyNowPrice = item.buy_now_price.toLocaleString();
+  // }
+
   return (
     <Box sx={{ mx: 2 }}>
-      <Button
-        variant="contained"
-        // color="primary"
-        onClick={() => {
-          if (onBuyNowClick) {
-            onBuyNowClick();
-          }
-        }}
-        disabled={item.status === "sold" && user?.id !== item.leading_user_id}
-        sx={{
-          color: "#fff",
-          width: "100%",
-          marginTop: 2,
-          borderRadius: 100,
-          backgroundColor: "#ffb81d",
-          boxShadow: "none",
-          "&:hover": {
+      {item.buy_now_price && (
+        <Button
+          variant="contained"
+          // color="primary"
+          onClick={() => {
+            if (onBuyNowClick) {
+              onBuyNowClick();
+            }
+          }}
+          disabled={item.status === "sold" && user?.id !== item.leading_user_id}
+          sx={{
+            color: "#fff",
+            width: "100%",
+            marginTop: 2,
+            borderRadius: 100,
+            backgroundColor: "#ffb81d",
             boxShadow: "none",
-          },
-          "&:active": {
-            boxShadow: "none",
-          },
-          "&:focus": {
-            boxShadow: "none",
-          },
-        }}
-      >
-        {item.status === "sold" && item.leading_user_id === user?.id
-          ? `You purchased for $${item.final_purchase_price}!`
-          : `Buy Now for $${item.buy_now_price.toLocaleString()}`}
-      </Button>
+            "&:hover": {
+              boxShadow: "none",
+            },
+            "&:active": {
+              boxShadow: "none",
+            },
+            "&:focus": {
+              boxShadow: "none",
+            },
+          }}
+        >
+          {item.status === "sold" && item.leading_user_id === user?.id
+            ? `You purchased for $${item.final_purchase_price}!`
+            : `Buy Now for $${item.buy_now_price.toLocaleString()}`}
+        </Button>
+      )}
     </Box>
   );
 }
